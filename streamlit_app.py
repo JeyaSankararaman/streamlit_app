2a9b6ea1669063c550c19bf3ac0c079f2c82ac04
Jeyasutha Sankararaman <124129045+JeyaSankararaman@users.noreply.github.com>
2023-10-05 13:49:33 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,11 +21,8 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
-def add_fruit():
-    fruit_added = streamlit.text_input('What fruit would you like to add?')
-    streamlit.write('Thankyou for adding', fruit_added)
     
-streamlit.button("+", key=None, help=None, on_click=add_fruit)
+# streamlit.button("+", key=None, help=None, on_click=add_fruit)
 
     
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
@@ -47,4 +44,5 @@ my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
-
+fruit_added = streamlit.text_input('What fruit would you like to add?')
+streamlit.write('Thankyou for adding', fruit_added)
